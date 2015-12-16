0a0fb95c8dd185d64ede157c00092f88afc79ca3
Rui Neves Carvalho <astralenigma@hotmail.com>
2015-12-16 23:38:50 +0000

    Uma pequena limpeza nos triggers.

--- SQLQueryProjTrig.sql
+++ SQLQueryProjTrig.sql
@@ -27,42 +27,9 @@ BEGIN
    HistoricoValorLicitacao,HistoricoDataCompetLicitacao ,HistoricoProdutoID, HistoricoLicitacaoID
    )values(@valor, getdate(),@produto,@licitacao)
 
-	update SchemaProduto.Produto
-	set ProdutoValorActual= @valor where ProdutoId=@produto
+	--update SchemaProduto.Produto
+	--set ProdutoValorActual= @valor where ProdutoId=@produto
 	
-	update Licitacao
-	set LicitacaoValorActual = @valor where LicitacaoId =@licitacao
 END
 Go
 
-CREATE TRIGGER SchemaProduto.TrHistorico
-ON SchemaProduto.Historico
-AFTER INSERT 
-AS
-BEGIN
-	Declare @produto int
-	Declare @licitacao int
-	Declare @valor decimal
-	Declare @valorNov decimal
-	Declare @valorMax decimal
-	if exists (select 1 from SchemaLicitacao.Licitacao, inserted 
-		where HistoricoProdutoID=LicitacaoProdutoID and HistoricoLicitacaoID!=LicitacaoID )
-	Begin
-		select @valorMax=Max( LicitacaoValorMax ) from SchemaLicitacao.Licitacao, inserted where LicitacaoID != HistoricoLicitacaoID
-		select @licitacao=LicitacaoId from SchemaLicitacao.Licitacao where LicitacaoValorMax=@valorMax
-		select @valor=(HistoricoValorLicitacao+0.01), @produto=HistoricoProdutoID from inserted
-		if @valor<=@valorMax
-		begin
-			insert into SchemaProduto.Historico(
-				HistoricoValorLicitacao,HistoricoDataCompetLicitacao,HistoricoProdutoID, HistoricoLicitacaoID
-				)values(@valor, GETDATE(), @produto,@licitacao)
-			update SchemaProduto.Produto
-			set ProdutoValorActual= @valor where ProdutoId=@produto
-			
-			update Licitacao
-			set LicitacaoValorActual = @valor where LicitacaoId =@licitacao
-		end 
-	end
-	
- END
-Go
