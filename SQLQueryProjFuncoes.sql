0e44ff7fc34990e1eb5d2261910c7bfbf8ab3c1c
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-01-09 02:49:16 +0000

    Pelos vistos é um erro levantar erro nas funções. E correcção na limpeza.

--- SQLQueryProjFuncoes.sql
+++ SQLQueryProjFuncoes.sql
@@ -3,7 +3,7 @@ Go
 
 --Funções que devem funcionar.--
 --Converte a password para uma hash--
-IF OBJECT_ID ('SchemaUtilizador.funcPassToHash', 'FS') IS NOT NULL
+IF OBJECT_ID (N'SchemaUtilizador.funcPassToHash', N'FN') IS NOT NULL
 	DROP function SchemaUtilizador.funcPassToHash;
 GO
 CREATE FUNCTION SchemaUtilizador.funcPassToHash (@pass NVARCHAR)
@@ -20,7 +20,7 @@ GO
 --select SchemaUtilizador.funcPassToHash('OAS53QMI5JS')/*exemplo que o mais precisa-se no projeto*/
 select SchemaUtilizador.funcPassToHash('password1')/*exemplo que o mais precisa-se no projeto*/
 */
-IF OBJECT_ID ('SchemaUtilizador.funcIdadeTens', 'FS') IS NOT NULL
+IF OBJECT_ID (N'SchemaUtilizador.funcIdadeTens', N'FN') IS NOT NULL
 	DROP function SchemaUtilizador.funcIdadeTens;
 GO
 --Calcular a idade a partir da data --/* sofreu a alteração na aula de Lab*/
@@ -50,7 +50,7 @@ GO
 
 
 --Compara a password do utilizador (usar em logins)--
-IF OBJECT_ID ('SchemaUtilizador.funcPassConfirm', 'FS') IS NOT NULL
+IF OBJECT_ID (N'SchemaUtilizador.funcPassConfirm', N'FN') IS NOT NULL
 	DROP function SchemaUtilizador.funcPassConfirm;
 GO
 CREATE FUNCTION SchemaUtilizador.funcPassConfirm(@user int, @pass NVARCHAR)
@@ -67,36 +67,10 @@ BEGIN
 END;
 Go
 
---Compara 2 valores e vê o maior 0=erro, 1=Licitacao Velha e 2=Licitacao Nova
-IF OBJECT_ID ('SchemaLicitacao.funcCompLicita', 'FS') IS NOT NULL
-	DROP function SchemaLicitacao.funcCompLicita;
-GO
-Create FUNCTION SchemaLicitacao.funcCompLicita(@licitacaoNovaVal decimal,@licitacaoVelhaID int, @prodID int)
-returns int
-as
-BEGIN
-	DECLARE @prodVal decimal(9,2)
-	DECLARE @licitacaoVelhaVal decimal(9,2)
-	DECLARE @licitacaoVelhaValMax decimal(9,2)
-	select @prodVal=ProdutoValorMinVenda from SchemaProduto.Produto where ProdutoId=@prodID
-	if(@prodVal> @licitacaoNovaVal)
-	begin
-		return 0
-	end
-	select @licitacaoVelhaVal= LicitacaoValorActual, @licitacaoVelhaValMax=Max(LicitacaoValorMax) from Licitacao where LicitacaoProdutoID=@prodID
-	if(@licitacaoVelhaValMax<@licitacaoNovaVal)
-	begin
-		return 2
-	end
-	else
-	begin
-		return 1
-	end
-END
-Go
+
 
 --Calcular a licitacao a partir de 2 valores 
-IF OBJECT_ID ('SchemaLicitacao.funcCompLicita', 'FS') IS NOT NULL
+IF OBJECT_ID (N'SchemaLicitacao.funcCompLicita', N'FN') IS NOT NULL
 	DROP function SchemaLicitacao.funcCompLicita;
 GO
 Create FUNCTION SchemaLicitacao.funcCompLicita(@licitacaoNovaVal decimal,@licitacaoVelha decimal)
@@ -122,76 +96,3 @@ Begin
 	return @return 
 end
 go
-
---Funcoes de apoio ao procLicitarProd--
-IF OBJECT_ID ('SchemaLicitacao.CheckProduto', 'FS') IS NOT NULL
-	DROP function SchemaLicitacao.CheckProduto;
-GO
-Create FUNCTION SchemaLicitacao.CheckProduto(@prodID int)
-returns int
-as
-Begin
-	Declare @msgErro varchar(500)
-	if not exists (Select 1 from SchemaProduto.Produto where ProdutoId=@prodID)
-	begin 
-		set @msgErro = 'O produto não se encontra nos registos.'
-		RAISERROR(@msgErro,16,1)
-		RETURN 0
-	end
-	Return 1
-End
-Go
-
-IF OBJECT_ID ('SchemaLicitacao.CheckDataLeilao', 'FS') IS NOT NULL
-	DROP function SchemaLicitacao.CheckDataLeilao;
-GO
-Create FUNCTION SchemaLicitacao.CheckDataLeilao(@prodDate datetime)
-returns int
-as
-Begin
-	Declare @msgErro varchar(500)
-	if datediff(s,getdate(),@prodDate)<0
-	begin
-		set @msgErro = 'Já passou o tempo para licitar. ' + CONVERT(VARCHAR, @prodDate)
-		RAISERROR(@msgErro,16,1)
-		RETURN 0
-	end
-	return 1
-end
-GO
-
-IF OBJECT_ID ('SchemaLicitacao.CheckValorProduto', 'FS') IS NOT NULL
-	DROP function SchemaLicitacao.CheckValorProduto;
-GO
-Create FUNCTION SchemaLicitacao.CheckValorProduto(@ProdVal decimal(9,2),@licitaValMax decimal(9,2))
-returns int
-as
-Begin
-	Declare @msgErro varchar(500)
-	if (@ProdVal>@licitaValMax)
-	begin
-		set @msgErro = 'O valor da licitação é menor que o valor mínimo.'
-		RAISERROR(@msgErro,16,1)
-		RETURN 0
-	end
-	RETURN 1
-END
-GO
-
-IF OBJECT_ID ('SchemaLicitacao.CheckUtilizador', 'FS') IS NOT NULL
-	DROP function SchemaLicitacao.CheckUtilizador;
-GO
-Create FUNCTION SchemaLicitacao.CheckUtilizador(@NuserID int)
-returns int
-as
-Begin
-	Declare @msgErro varchar(500)
-	if not exists (Select 1 from SchemaUtilizador.Utilizador where UtilizadorId=@NuserID)
-	begin
-		set @msgErro = 'O utilizador não se encontra nos registos.'
-		RAISERROR(@msgErro,16,1)
-		RETURN 0
-	end
-	RETURN 1
-END
-GO
