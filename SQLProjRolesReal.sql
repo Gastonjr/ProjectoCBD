fa15d28ecb9825342316a6f62911d8751f256f3c
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-01-16 17:32:46 +0000

    Adicionadas mais umas permissões.

--- SQLProjRolesReal.sql
+++ SQLProjRolesReal.sql
@@ -13,10 +13,17 @@ GRANT Select on SCHEMA::SchemaLicitacao to [Administrador]
 GO
 GRANT Select on SCHEMA::SchemaProduto to [Administrador]
 GO
+
 Grant Select on SchemaUtilizador.vUtililizadorLicitacaoCompra to [Gestor Financeiro]
 GO
+Grant execute on SchemaUtilizador.UtilizadoresMelhorClassificacaoMes to [Gestor Financeiro]
+GO
+Grant execute on SchemaUtilizador.UtilizadoresMelhorClassificacao to [Gestor Financeiro]
+Go
 
 Grant Select on SchemaProduto.vUtilizadorProvendoAvenda to [Utilizador]
 GO
 Grant execute on SchemaLicitacao.procLicitarProd to [Utilizador]
+GO
+Grant execute on SchemaUtilizador.ClassificarCompra to [Utilizador]
 GO
